9eb611890df5828c19cd29a8f35b72cc2a0bdf33
fernandogs <fernandogs1989@gmail.com>
2017-05-17 17:59:44 -0300

    init

--- esb/esb/esb.cpp
+++ esb/esb/esb.cpp
@@ -4,22 +4,32 @@
 #include "stdafx.h"
 #include "log.h"
 
+/*
+BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+BOOST_LOG_TRIVIAL(error) << "An error severity message";
+BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+*/
+int init() {
+	core::log::get().init("esb", trace);
+	return true;
+}
 
 int main(int argc, char* argv[])
 {
-	// init log
-	core::log::get().init("esb", trace);
+	BOOST_LOG_NAMED_SCOPE(__FUNCTION__);
 
-	BOOST_LOG_NAMED_SCOPE("main");
-
-	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-	BOOST_LOG_TRIVIAL(error) << "An error severity message";
-	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+	if (!init()) {
+		return 1;
+	}
+	
+	BOOST_LOG_TRIVIAL(debug) << "Begin";
 
 
+
+	BOOST_LOG_TRIVIAL(debug) << "End";
 	return 0;
 }
 
