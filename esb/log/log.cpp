beabec3127ef8c24e94808df6c04f09bbbf2af11
fernandogs <fernandogs1989@gmail.com>
2017-05-19 18:14:02 -0300

    example config

--- esb/log/log.cpp
+++ esb/log/log.cpp
@@ -31,13 +31,22 @@ namespace core {
 	void log::release() {
 	}
 
+	/*
+	/* log.ini Example:
+	/* [Log]
+	/* Filename=log_%P_%Y-%m-%d-%H-%M-%S_%N.log
+	/* Target=log
+	/* RotationSize=10000
+	/* Format=[%TimeStamp%][%Scope%][%Message%]
+	/* Level=trace
+	*/
 	void log::init(const std::string& process, const std::string& ini){
 
 		load(process, ini);
 
 		logging::add_file_log(
 			keywords::file_name = _file_name, // "log_" + process + "_%Y-%m-%d-%H-%M-%S_%N.log",
-			keywords::target = _target, // "C:/"
+			keywords::target = _target, // "log"
 			keywords::rotation_size = _rotation_size, // 1000000 * 1024 + 1024,
 			keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
 			keywords::format = _format // "[%TimeStamp%][%Scope%][%Message%]",
