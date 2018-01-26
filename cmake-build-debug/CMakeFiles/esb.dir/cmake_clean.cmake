b7316dfb1228d4fb3f9d089d7ef831977a5d7e25
fernandogs <fernandogs1989@gmail.com>
2018-01-26 17:28:24 -0200

    creating cmak

---
+++ cmake-build-debug/CMakeFiles/esb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esb.dir/main.cpp.obj"
+  "esb.pdb"
+  "esb.exe"
+  "esb.exe.manifest"
+  "esb.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
